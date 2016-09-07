12e2ab2691bb425acac61d409280371e2778dde8
utkarshkum <utkarshkum@gmail.com>
2016-09-07 08:48:30 +0530

    Update ServicesFactory.java
    Updating the ip

--- src/main/java/com/cisco/util/ServicesFactory.java
+++ src/main/java/com/cisco/util/ServicesFactory.java
@@ -23,7 +23,7 @@ public class ServicesFactory {
 		}
 		
 		if(mongoTL.get()==null){
-			MongoClientURI connectionString = new MongoClientURI("mongodb://104.154.72.172:27017");
+			MongoClientURI connectionString = new MongoClientURI("mongodb://146.148.43.98:27017");
 			MongoClient mongoClient = new MongoClient(connectionString);	
 			Morphia morphia = new Morphia();
 			morphia.mapPackage("com.cisco.model");
